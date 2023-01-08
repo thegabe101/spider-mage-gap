5c0499f9c64423b1de1639f55dd2c0d3876be6a6
Rayncor <sowa332@gmail.com>
2023-01-08 10:52:17 -0800

    added footer

--- spider-redux/src/Router.jsx
+++ spider-redux/src/Router.jsx
@@ -1,38 +1,36 @@
-
-
-import {Routes, Route } from 'react-router-dom';
-import { HomeScreen } from './screens/HomeScreen'
-import { MageScreen } from './screens/MageScreen'
-import { BattleScreen } from './screens/BattleScreen'
-
-import NavBar from './components/navbar';
+import { Routes, Route } from "react-router-dom";
+import { HomeScreen } from "./screens/HomeScreen";
+import { MageScreen } from "./screens/MageScreen";
+import { BattleScreen } from "./screens/BattleScreen";
+import Footer from "./components/Foot";
+import NavBar from "./components/navbar";
 
 const Router = () => {
-    return (
-      <>
-        <NavBar />
-        <Routes>
-          <Route path ="/" element={<HomeScreen />} />
-          <Route path ="/mage" element={<MageScreen />} />
-          <Route path ="/battle" element={<BattleScreen />} />
-        </Routes>
-      </>
-
-    );
-  };
-  export default Router;
+	return (
+		<>
+			<NavBar />
+			<Routes>
+				<Route path="/" element={<HomeScreen />} />
+				<Route path="/mage" element={<MageScreen />} />
+				<Route path="/battle" element={<BattleScreen />} />
+			</Routes>
+			<Footer />
+		</>
+	);
+};
+export default Router;
 
 // const Router = () => {
 //     return (
 //         <Routes>
 //         <Route path='/' element={<HomeScreen />}>
 //           {/* <Route path='about' element={<About />} /> */}
-    
+
 //           {/* <Route path='products' element={<SharedProductLayout />}>
 //             <Route index element={<HomeScreen />} />
 //             <Route path=':productId' element={<SingleProduct />} />
 //           </Route> */}
-    
+
 //         </Route>
 //       </Routes>
 //     );

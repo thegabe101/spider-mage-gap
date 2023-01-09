9b118e57f6fd67b9c9c7aedee5a9160e6109b63f
Rayncor <sowa332@gmail.com>
2023-01-09 13:25:39 -0800

    commit to stash

--- scripts/seed.sh
+++ scripts/seed.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-docker exec -it spider-mage-gap_api_1 python3 /usr/src/app/seed_data.py
+docker exec -it api python3 /usr/src/app/seed_data.py
--- spider-redux/src/features/mages/Mage.jsx
+++ spider-redux/src/features/mages/Mage.jsx
@@ -2,13 +2,9 @@ import { useSelector } from "react-redux";
 import { selectAllMages } from "./magesSlice";
 
 const Mage = () => {
-    console.log("dfda")
+	console.log("dfda");
 
-    let content = "mage"
-    return (
-        <section>
-            {content}
-        </section>
-    )
-}
-export default Mage
+	let content = "mage";
+	return <section>{content}</section>;
+};
+export default Mage;

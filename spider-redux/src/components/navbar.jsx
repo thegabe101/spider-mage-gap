22b01d8170ee042ce6e7b4760de09651567b33f7
Rayncor <sowa332@gmail.com>
2023-01-08 22:06:04 -0800

    style containers, name handle

--- spider-redux/src/components/navbar.jsx
+++ spider-redux/src/components/navbar.jsx
@@ -5,11 +5,14 @@ import Tab from "@mui/material/Tab";
 import { Link } from "react-router-dom";
 import Box from "@mui/material/Box";
 import "../styles/navbar.css";
-// import fantback1 from "../assets/fantback1";
-// import fantback2 from "../assets/fantback2";
-// import fantback3 from "../assets/fantback3";
-// import fantback4 from "../assets/fantback4";
-// import fantback5 from "../assets/fantback5";
+// import { createTheme } from "@mui/material/styles";
+// import { light } from "@mui/material/styles/createPalette";
+
+// const theme = createTheme({
+// 	palette: {
+// 		primary: light,
+// 	},
+// });
 
 const NavBar = (props) => {
 	const [value, setValue] = useState(0);
@@ -26,8 +29,8 @@ const NavBar = (props) => {
 						value={value}
 						onChange={handleChange}
 						aria-label="Navigation"
-						indicatorColor="primary"
-						textColor="primary"
+						indicatorColor="secondary"
+						textColor="primary.light"
 					>
 						<Tab label="Home" index={0} component={Link} to={"/"} />
 						<Tab
--- spider-redux/src/redux/actions/index.js
+++ spider-redux/src/redux/actions/index.js
@@ -1,7 +1,6 @@
 import { SET_MAGE_FIRST_NAME, SET_MAGE_LAST_NAME } from "../types";
 export const updateMageFirstName = (firstName) => (dispatch, getState) => {
     try {
-        console.log(firstName, getState());
         dispatch({
             type: SET_MAGE_FIRST_NAME,
             payload: {
@@ -13,13 +12,9 @@ export const updateMageFirstName = (firstName) => (dispatch, getState) => {
         console.log("Error", error);
     }
 };
+
 export const updateMageLastName = (lastName) => (dispatch, getState) => {
     try {
-        // console.log(lastName);
-        // console.log(nameReducer.mageFirstName);
-        // console.log(nameReducer.mageLastName);
-        // console.log(firstName);
-        // console.log(lastName);
         dispatch({
             type: SET_MAGE_LAST_NAME,
             payload: {
